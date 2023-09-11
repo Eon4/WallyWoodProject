830dcfdaa2cb47659d8ace4b70160734655491ef
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-11 10:47:24 +0200

    update

---
+++ wood/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import style from '../styles/header.module.scss'
+import HeaderImg from  '../assets/images/seats.jpg'
+
+export function Header(){
+    return (
+        <header>
+            <h1 className={style.title}>WALLYWOOD</h1>
+            <img className={style.Headerimg} src={HeaderImg} alt="header" />
+
+        </header>
+    )
+}
--- wood/src/Pages/MainPage.jsx
+++ wood/src/Pages/MainPage.jsx
@@ -1,10 +1,12 @@
 
 import { Navigation } from "../Components/Navigation";
+import { Header } from "../Components/Header";
 
 export const MainPage = () => {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 };
